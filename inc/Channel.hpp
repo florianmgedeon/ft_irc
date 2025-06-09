1e937494873a9677416c88d99d6a57a6803e55f7
skoruf <chris.thaler89@gmail.com>
2025-06-09 14:34:04 +0200

    mode pointer fcts implemented

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -10,15 +10,30 @@ class Channel
 {
     private:
 		std::string 		_name;
+        typedef void(Channel::*cmd_t)(std::string&);
+        typedef std::map<std::string, cmd_t> commandMap_t;
+        typedef commandMap_t::iterator  commandIter;
         std::string         _topic, _topicSetter;
         time_t				_topicTimestamp;
         std::string         _password;
-        //int                 _userLimit;
-        //bool                _inviteOnly;
-        //bool                _topicRestricted;
+        int                 _userLimit;
+        bool                _inviteOnly;
+        bool                _topicRestricted;
         bool                _hasPassword;
+        std::vector<std::string> _invites;
         std::vector<std::string> _members;
         std::vector<std::string> _operators;
+	commandMap_t                    _commandMap;
+        void		iPlus	(std::string &line);
+        void		iMinus	(std::string &line);
+        void		tPlus	(std::string &line);
+        void		tMinus	(std::string &line);
+        void		kPlus	(std::string &line);
+        void		kMinus	(std::string &line);
+        void		oPlus	(std::string &line);
+        void		oMinus	(std::string &line);
+        void		lPlus	(std::string &line);
+        void		lMinus	(std::string &line);
 
     public:
         Channel();
@@ -26,6 +41,7 @@ class Channel
         Channel(std::string c);
         Channel(std::string c, std::string pwd);
 
+	bool executeMode(std::string modestring, std::string argument);
         bool addMember(std::string c);
         bool isMember(std::string nick);
         void removeMember(std::string nick);
@@ -37,11 +53,13 @@ class Channel
         bool isEmpty();
         void sendChannelMessage(std::string sender, std::string message, std::vector<Client> &clients);
         bool checkPassword(std::string in);
-
+	
         const std::string& getTopic() const;
 		std::string	memberlist();
 		void setTopic(const std::string &topic, const std::string &setter);
 		bool hasTopic();
+		bool hasInvite(std::string nick);
+		bool hasInviteOnly();
 		std::string getTopicTimestamp() const;
 		const std::string& getTopicSetter() const;
 };
--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -2,27 +2,136 @@
 
 Channel::Channel() {
 	_topicTimestamp = 0;
-	_hasPassword = false;
+	//_userLimit = 0;
+	//_hasPassword = false;
+	//_inviteOnly = false;
+	//_topicRestricted = false;
 	_members.empty();
 	_operators.empty();
+	
 }
 
 Channel::~Channel() {}
 
 Channel::Channel(std::string c) {
+	_userLimit = 0;
+	_hasPassword = false;
+	_inviteOnly = false;
+	_topicRestricted = false;
+	_commandMap.insert(std::make_pair("+i",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-i",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+t",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-t",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+k",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-k",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+o",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-o",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+l",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-l",	&Channel::iMinus));
 	Channel();
 	_members.push_back(c);
 	addOperator(c);
 }
 
 Channel::Channel(std::string c, std::string pwd): _password(pwd) {
+	_userLimit = 0;
+	_hasPassword = true;
+	_inviteOnly = false;
+	_topicRestricted = false;
+	_commandMap.insert(std::make_pair("+i",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-i",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+t",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-t",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+k",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-k",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+o",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-o",	&Channel::iMinus));
+	_commandMap.insert(std::make_pair("+l",	&Channel::iPlus));
+	_commandMap.insert(std::make_pair("-l",	&Channel::iMinus));
 	Channel();
 	_members.push_back(c);
 	addOperator(c);
-	_hasPassword = true;
 }
 
 //----------------------MEMBERS----------------------------
+
+bool Channel::executeMode(std::string modestring, std::string argument){
+	
+	commandIter comMapIt = _commandMap.find(modestring);	
+			if (comMapIt != _commandMap.end()){
+				(this->*(comMapIt->second))(argument);
+				return true;
+	}
+	return false;
+}
+
+//TODO: check for requirements (must or must not have argument)
+void Channel::iPlus(std::string& argument){
+	(void)argument;
+	
+	_inviteOnly = true;
+}
+
+void Channel::iMinus(std::string& argument){
+	(void)argument;
+	
+	_inviteOnly = false;
+}
+
+void Channel::tPlus(std::string& argument){
+	(void)argument;
+	
+	_topicRestricted = true;
+}
+
+void Channel::tMinus(std::string& argument){
+	(void)argument;
+	
+	_topicRestricted = false;
+}
+
+void Channel::kPlus(std::string& argument){
+
+	_hasPassword = true;
+	_password = argument;
+}
+
+void Channel::kMinus(std::string& argument){
+	(void)argument;
+	
+	//TODO: must have an argument, vermutl compare mit current pw
+	_hasPassword = false;
+}
+
+void Channel::oPlus(std::string& argument){
+
+	if (isMember(argument) && !isOperator(argument))
+		addOperator(argument);
+}
+
+void Channel::oMinus(std::string& argument){
+	//TODO: verhindern, dass founder removed wird, sich selbst removen?
+	//evtl in commands handeln
+	if (isOperator(argument))
+		removeOperator(argument);
+}
+
+void Channel::lPlus(std::string& argument){
+	
+	if (atoi(argument.c_str()) > 0 && atoi(argument.c_str()) ==
+		static_cast<int>(atoll(argument.c_str())))
+		_userLimit = atoi(argument.c_str());
+}
+
+void Channel::lMinus(std::string& argument){
+	(void)argument;
+	//TODO: must not have an argument
+	_userLimit = 0;
+}
+
+bool Channel::hasInviteOnly(){
+	return _inviteOnly;
+}
 
 bool Channel::addMember(std::string c) {
 	if (!isMember(c))
@@ -57,6 +166,10 @@ void Channel::removeOperator(std::string nick) {
 }
 
 //------------------------------------------------
+
+bool Channel::hasInvite(std::string nick){
+	return std::find(_invites.begin(), _invites.end(), nick) != _invites.end();
+}
 
 bool Channel::isEmpty() {return _members.empty();}
 

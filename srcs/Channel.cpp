1eb01bc2d3ba3064a3ab177792ce7b18f341f22f
Christoph Thaler <cthaler@c3r9p5.42vienna.com>
2025-06-17 15:49:01 +0200

    fix out of bounds

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -42,11 +42,11 @@ bool isNumber(std::string& str){
 
 void Channel::lastOp(std::vector<Client>::iterator c, std::vector<Client> &clients){
 
-	std::string client = _members[1];
+	std::string client = _members[0];
 	if (_members.size() > 1 && isOperator(c->getNickname())
 		&& _operators.size() == 1){
-		if (_members[0] != c->getNickname())
-			client = _members[0];
+		if (_members[0] == c->getNickname())
+			client = _members[1];
 		sendChannelMessage(c->getNickname(), c->getNickUserHost() + " MODE #" +
 			_name + " +o " + client, clients);
 	}
